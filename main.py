493d71b62c4d9ea586393f2ab4d22d751619a734
NoeL64 <64926297+NoeL64@users.noreply.github.com>
2020-06-26 14:36:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 menu = int(input("Options \n 1: Save to DB \n 2: Get from DB"))
- 
-
+#importing replit dB
+#explorer only (I think)
+#thx codermoney 51 for making this module 
 import replitdb
 client = replitdb.Client()
 if menu == 1:
-  name = input("What would you like to name your save in the database")
-  value = input("What would you like to assign it to?")
+  name = input("What would you like to name your save in the database\n")
+  value = input("What would you like to assign it to?\n")
   client.add(name=value)
- else:
+ elif menu == 2:
+  name = input("What name would you like to get from the database?\n")
+  print(client.view(name))
+else:
+  print("not an option")
